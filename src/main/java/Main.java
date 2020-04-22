f483851aec2d0b66d9248ada649776e53ff0bde4
SergeyTy <seregio2002@outlook.com>
2020-04-22 13:38:57 +0700

    add class Media

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,3 +3,18 @@ public class Main {
         System.out.println("Hello world!");
     }
 }
+class Media // класс для хранения данных о файле
+{
+    public byte[] hash; // хэш
+    public String path; // путь
+    public long v; //объём
+
+    @Override
+    public String toString() {
+        return "Media{" +
+                "hash=" + Arrays.toString(hash) +
+                ", path='" + path + '\'' +
+                ", v=" + v +
+                '}';
+    }
+}
