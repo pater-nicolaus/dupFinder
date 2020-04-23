1561dcaf013bf8e22327283d3ca71c2b5aca1b4e
SergeyTy <seregio2002@outlook.com>
2020-04-23 19:11:58 +0700

    change name of var

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,19 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    class data_of_file    {
+        public byte[] hash;
+        public Path path;
+        public long length;
+
+        @Override
+        public String toString() {
+            return "Media{" +
+                    "hash=" + Arrays.toString(hash) +
+                    ", path='" + path + '\'' +
+                    ", v=" + length +
+                    '}';
+        }
+    }
 }
