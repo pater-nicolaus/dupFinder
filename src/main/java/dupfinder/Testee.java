4b535b3433e9a40d2a7414d6a7beefb215bd819a
Sergey <seregio2002@outlook.com>
2020-07-29 19:14:45 +0700

    new test for power3

--- src/main/java/dupfinder/Testee.java
+++ src/main/java/dupfinder/Testee.java
@@ -4,4 +4,8 @@ public class Testee {
     public static int power2(int n) {
         return n*n;
     }
+    public static int power3(int n) {
+        return n*n*n;
+    }
+
 }
--- src/test/java/dupfinder/TesteeTest.java
+++ src/test/java/dupfinder/TesteeTest.java
@@ -9,4 +9,8 @@ public class TesteeTest {
         Assert.assertEquals(4, Testee.power2(2));
         Assert.assertEquals(9, Testee.power2(3));
     }
+    public void power3_test() {
+        Assert.assertEquals(8, Testee.power3(2));
+        Assert.assertEquals(27, Testee.power3(3));
+    }
 }
