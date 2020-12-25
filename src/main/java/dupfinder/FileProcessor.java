947b9f459b1373a5877fc7bbba38aea03fdbe73c
Николай Симонов <simonov@inetra.ru>
2020-12-25 22:22:18 +0700

    use MappedByteBuffer; fix tests

--- src/main/java/dupfinder/FileProcessor.java
+++ src/main/java/dupfinder/FileProcessor.java
@@ -1,6 +1,11 @@
 package dupfinder;
 
-import java.io.*;
+import sun.nio.ch.DirectBuffer;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 import java.nio.file.Path;
 
 
@@ -11,22 +16,24 @@ abstract public class FileProcessor {
     public FileProcessor(Path path, int bufferSize) {
         this.path = path;
         this.buffer = new byte[bufferSize];
-
-
     }
 
     public void processFile() throws IOException {
-        int bytesRead;
-
-        try (FileInputStream f = new FileInputStream(path.toString())) {
-            do {
-                bytesRead = f.read(buffer, 0, buffer.length);
-                if (bytesRead != -1)
-                    processFilePart(buffer, bytesRead);
-            } while (bytesRead != -1);
+        try (RandomAccessFile file = new RandomAccessFile(path.toFile(), "r")) {
+            FileChannel fileChannel = file.getChannel();
+            //KLUDGE: here we can read only files less than 2GB. If we need to read bigger files we should move the mapping window.
+            MappedByteBuffer mappedByteBuffer = fileChannel.map(FileChannel.MapMode.READ_ONLY, 0, fileChannel.size());
+            try {
+                while (mappedByteBuffer.hasRemaining()) {
+                    int bytesToRead = Math.min(buffer.length, mappedByteBuffer.remaining());
+                    mappedByteBuffer.get(buffer, 0, bytesToRead);
+                    processFilePart(buffer, bytesToRead);
+                }
+            } finally {
+                ((DirectBuffer) mappedByteBuffer).cleaner().clean();
+            }
         }
     }
-
 
     abstract void processFilePart(byte[] data, int size);
 }
--- src/test/java/dupfinder/FileProcessorTest.java
+++ src/test/java/dupfinder/FileProcessorTest.java
@@ -1,5 +1,7 @@
 package dupfinder;
 
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 import org.mockito.Mockito;
 import org.springframework.util.FileSystemUtils;
 import org.testng.annotations.AfterClass;
@@ -9,14 +11,18 @@ import org.testng.annotations.Test;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.fail;
 
 public class FileProcessorTest {
     Path tempDir;
 
     static class FileProcessorFake extends FileProcessor {
+        List<Pair<byte[], Integer>> parts = new ArrayList<>();
 
         public FileProcessorFake(Path path, int bufferSize) {
             super(path, bufferSize);
@@ -24,6 +30,7 @@ public class FileProcessorTest {
 
         @Override
         public void processFilePart(byte[] data, int size) {
+            parts.add(new ImmutablePair<>(data.clone(), size));
         }
     }
 
@@ -43,10 +50,13 @@ public class FileProcessorTest {
     public void testSingleBlock() {
         try {
             TestFile testFile = new TestFile(tempDir, "file1", 100);
-            fileProcessor = Mockito.spy(new FileProcessorFake(testFile.getPath(), 100));
-            fileProcessor.processFile();
-            Mockito.verify(fileProcessor).processFilePart(testFile.getData(), testFile.getLength());
-            testFile.delete();
+            try {
+                fileProcessor = Mockito.spy(new FileProcessorFake(testFile.getPath(), 100));
+                fileProcessor.processFile();
+                Mockito.verify(fileProcessor).processFilePart(testFile.getData(), testFile.getLength());
+            } finally {
+                testFile.delete();
+            }
         } catch (IOException e) {
             fail("Unexpected exception", e);
         }
@@ -56,15 +66,24 @@ public class FileProcessorTest {
     public void testMultipleBlocks() {
         try {
             TestFile testFile = new TestFile(tempDir, "file2", 100);
-            fileProcessor = Mockito.spy(new FileProcessorFake(testFile.getPath(), 50));
-            fileProcessor.processFile();
-            byte[] part1 = Arrays.copyOfRange(testFile.getData(), 0, 50);
-            byte[] part2 = Arrays.copyOfRange(testFile.getData(), 50, 100);
-            Mockito.verify(fileProcessor).processFilePart(part1, 50);
-            Mockito.verify(fileProcessor).processFilePart(part2, 50);
-            testFile.delete();
+            try {
+                fileProcessor = Mockito.spy(new FileProcessorFake(testFile.getPath(), 50));
+                fileProcessor.processFile();
+                byte[] part1 = Arrays.copyOfRange(testFile.getData(), 0, 50);
+                byte[] part2 = Arrays.copyOfRange(testFile.getData(), 50, 100);
+                assertEquals(2, fileProcessor.parts.size());
+                assertArrayEquals(part1, fileProcessor.parts.get(0).getLeft(), fileProcessor.parts.get(0).getRight());
+            } finally {
+                testFile.delete();
+            }
         } catch (IOException e) {
             fail("Unexpected exception", e);
+        }
+    }
+
+    private void assertArrayEquals(byte[] part1, byte[] left, Integer right) {
+        for (int i = 0; i < right; i++) {
+            assertEquals(part1[i], left[i]);
         }
     }
 }
